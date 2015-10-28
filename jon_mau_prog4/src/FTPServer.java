3b2f218c8f7c6e44dd7eefa6d17f0fd9cab52749
heyitsmattjones <callmejones2011@gmail.com>
2015-10-28 16:30:18 -0500

    Added psvm and run to FTPServer

--- jon_mau_prog4/src/FTPServer.java
+++ jon_mau_prog4/src/FTPServer.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
 
 @author Pauer
 */
 public class FTPServer
 {
-
+   private final int LISTEN_PORT = 5721;
+   
+   public static void main(String[] args)
+   {
+      try
+      {
+         FTPServer fileServ = new FTPServer();
+         fileServ.run();
+      }
+      catch(Exception e)
+      {
+         e.toString();
+      }
+   }
+   
+   public void run()
+   {
+      
+   }
 }
