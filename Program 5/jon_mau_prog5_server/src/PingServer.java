60ef0bf21337e5a08446aace1a76db89a2e63b51
pauer94 <pauer94@aim.com>
2015-11-14 13:32:50 -0600

    Added PingMessage to Prog5 client

--- Program 5/jon_mau_prog5_server/src/PingServer.java
+++ Program 5/jon_mau_prog5_server/src/PingServer.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
+Program 5 consists of a basic UDP server and client.
+@author Paul Mauer & Matt Jones
+*/
 
-@author Pauer
+/*
+
 */
 public class PingServer
 {
-
+   ;
 }
